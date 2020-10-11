77a7d3c4ce3ff6293c302229950991f884cc2fc7
nmarkko <50952339+nmarkko@users.noreply.github.com>
2020-10-11 20:44:40 +0200

    Add files via upload

---
+++ Drivers/dma_driver/dma_driver.c
@@ -0,0 +1,406 @@
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
+#include <linux/device.h>
+
+#include <linux/io.h> //iowrite ioread
+#include <linux/slab.h>//kmalloc kfree
+#include <linux/platform_device.h>//platform driver
+#include <linux/of.h>//of_match_table
+#include <linux/ioport.h>//ioremap
+
+#include <linux/dma-mapping.h>  //dma access
+#include <linux/mm.h>  //dma access
+#include <linux/interrupt.h>  //interrupt handlers
+
+MODULE_AUTHOR ("FTN");
+MODULE_DESCRIPTION("Test Driver for VGA controller IP.");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_ALIAS("custom:image controller");
+
+#define DEVICE_NAME "image"
+#define DRIVER_NAME "image_driver"
+#define BUFF_SIZE 20
+#define MAX_PKT_LEN 640*480*4
+
+//*******************FUNCTION PROTOTYPES************************************
+static int image_probe(struct platform_device *pdev);
+static int image_open(struct inode *i, struct file *f);
+static int image_close(struct inode *i, struct file *f);
+static ssize_t image_read(struct file *f, char __user *buf, size_t len, loff_t *off);
+static ssize_t image_write(struct file *f, const char __user *buf, size_t length, loff_t *off);
+static ssize_t image_mmap(struct file *f, struct vm_area_struct *vma_s);
+static int __init image_init(void);
+static void __exit image_exit(void);
+static int image_remove(struct platform_device *pdev);
+
+static irqreturn_t dma_isr(int irq,void*dev_id);
+int dma_init(void __iomem *base_address);
+u32 dma_simple_write(dma_addr_t TxBufferPtr, u32 max_pkt_len, void __iomem *base_address); 
+
+//*********************GLOBAL VARIABLES*************************************
+struct image_info {
+  unsigned long mem_start;
+  unsigned long mem_end;
+  void __iomem *base_addr;
+  int irq_num;
+};
+
+static struct cdev *my_cdev;
+static dev_t my_dev_id;
+static struct class *my_class;
+static struct device *my_device;
+static struct image_info *vp = NULL;
+
+static struct file_operations my_fops =
+{
+	.owner = THIS_MODULE,
+	.open = image_open,
+	.release = image_close,
+	.read = image_read,
+	.write = image_write,
+	.mmap = image_mmap
+};
+
+static struct of_device_id image_of_match[] = {
+	{ .compatible = "xlnx,axi-dma-mm2s-channel", },
+	{ .compatible = "image"},
+	{ /* end of list */ },
+};
+MODULE_DEVICE_TABLE(of, image_of_match);
+
+static struct platform_driver image_driver = {
+	.driver = {
+		.name = DRIVER_NAME,
+		.owner = THIS_MODULE,
+		.of_match_table	= image_of_match,
+	},
+	.probe		= image_probe,
+	.remove	= image_remove,
+};
+
+
+dma_addr_t tx_phy_buffer;
+u32 *tx_vir_buffer;
+
+//***************************************************************************
+// PROBE AND REMOVE
+static int image_probe(struct platform_device *pdev)
+{
+	struct resource *r_mem;
+	int rc = 0;
+
+	printk(KERN_INFO "image_probe: Probing\n");
+	// Get phisical register adress space from device tree
+	r_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!r_mem) {
+		printk(KERN_ALERT "image_probe: Failed to get reg resource\n");
+		return -ENODEV;
+	}
+	// Get memory for structure image_info
+	vp = (struct image_info *) kmalloc(sizeof(struct image_info), GFP_KERNEL);
+	if (!vp) {
+		printk(KERN_ALERT "image_probe: Could not allocate memory for structure image_info\n");
+		return -ENOMEM;
+	}
+	// Put phisical adresses in timer_info structure
+	vp->mem_start = r_mem->start;
+	vp->mem_end = r_mem->end;
+
+	// Reserve that memory space for this driver
+	if (!request_mem_region(vp->mem_start,vp->mem_end - vp->mem_start + 1, DRIVER_NAME))
+	{
+		printk(KERN_ALERT "image_probe: Could not lock memory region at %p\n",(void *)vp->mem_start);
+		rc = -EBUSY;
+		goto error1;
+	}    
+	// Remap phisical to virtual adresses
+
+	vp->base_addr = ioremap(vp->mem_start, vp->mem_end - vp->mem_start + 1);
+	if (!vp->base_addr) {
+		printk(KERN_ALERT "image_probe: Could not allocate memory for remapping\n");
+		rc = -EIO;
+		goto error2;
+	}
+
+	// Get irq num 
+	vp->irq_num = platform_get_irq(pdev, 0);
+	if(!vp->irq_num)
+	{
+		printk(KERN_ERR "image_probe: Could not get IRQ resource\n");
+		rc = -ENODEV;
+		goto error2;
+	}
+
+	if (request_irq(vp->irq_num, dma_isr, 0, DEVICE_NAME, NULL)) {
+		printk(KERN_ERR "image_probe: Could not register IRQ %d\n", vp->irq_num);
+		return -EIO;
+		goto error3;
+	}
+	else {
+		printk(KERN_INFO "image_probe: Registered IRQ %d\n", vp->irq_num);
+	}
+
+	/* INIT DMA */
+	dma_init(vp->base_addr);
+	dma_simple_write(tx_phy_buffer, MAX_PKT_LEN, vp->base_addr); // helper function, defined later
+
+	printk(KERN_NOTICE "image_probe: VGA platform driver registered\n");
+	return 0;//ALL OK
+
+error3:
+	iounmap(vp->base_addr);
+error2:
+	release_mem_region(vp->mem_start, vp->mem_end - vp->mem_start + 1);
+	kfree(vp);
+error1:
+	return rc;
+
+}
+
+static int image_remove(struct platform_device *pdev)
+{
+	u32 reset = 0x00000004;
+	// writing to MM2S_DMACR register. Seting reset bit (3. bit)
+	printk(KERN_INFO "image_probe: resseting");
+	iowrite32(reset, vp->base_addr); 
+
+	free_irq(vp->irq_num, NULL);
+	iounmap(vp->base_addr);
+	release_mem_region(vp->mem_start, vp->mem_end - vp->mem_start + 1);
+	kfree(vp);
+	printk(KERN_INFO "image_probe: VGA DMA removed");
+	return 0;
+}
+
+//***************************************************
+// IMPLEMENTATION OF FILE OPERATION FUNCTIONS
+static int image_open(struct inode *i, struct file *f)
+{
+	//printk(KERN_INFO "image opened\n");
+	return 0;
+}
+
+static int image_close(struct inode *i, struct file *f)
+{
+	//printk(KERN_INFO "image closed\n");
+	return 0;
+}
+
+static ssize_t image_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+	//printk("image read\n");
+	return 0;
+}
+
+static ssize_t image_write(struct file *f, const char __user *buf, size_t length, loff_t *off)
+{	
+	char buff[BUFF_SIZE];
+	int ret = 0;
+	unsigned int xpos=0,ypos=0;
+	unsigned long long rgb=0;
+	unsigned char rgb_buff[10];  
+	ret = copy_from_user(buff, buf, length);  
+	if(ret){
+		printk("copy from user failed \n");
+		return -EFAULT;
+	}  
+	buff[length] = '\0';
+
+
+	sscanf(buff,"%d,%d,%s", &xpos, &ypos, rgb_buff);  
+	ret = kstrtoull(rgb_buff, 0, &rgb);
+
+	if(ret != -EINVAL) //checking for parsing error
+	{
+		if (xpos > 639)
+		{
+			printk(KERN_WARNING "VGA_write: X_axis position exceeded, maximum is 639 and minimum 0 \n");
+		}
+		else if (ypos > 479)
+		{
+			printk(KERN_WARNING "VGA_write: Y_axis position exceeded, maximum is 479 and minimum 0 \n");
+		}
+		else
+		{
+			tx_vir_buffer[640*ypos + xpos] = (u32)rgb;
+		}
+	}
+	else
+	{
+		printk(KERN_WARNING "VGA_write: Wrong write format, expected \"xpos,ypos,rgb\"\n");
+		// return -EINVAL; //parsing error
+	}        
+	return length;
+
+}
+
+static ssize_t image_mmap(struct file *f, struct vm_area_struct *vma_s)
+{
+	int ret = 0;
+	long length = vma_s->vm_end - vma_s->vm_start;
+
+	//printk(KERN_INFO "DMA TX Buffer is being memory mapped\n");
+
+	if(length > MAX_PKT_LEN)
+	{
+		return -EIO;
+		printk(KERN_ERR "Trying to mmap more space than it's allocated\n");
+	}
+
+	ret = dma_mmap_coherent(NULL, vma_s, tx_vir_buffer, tx_phy_buffer, length);
+	if(ret<0)
+	{
+		printk(KERN_ERR "memory map failed\n");
+		return ret;
+	}
+	return 0;
+}
+
+/****************************************************/
+// IMPLEMENTATION OF DMA related functions
+
+static irqreturn_t dma_isr(int irq,void*dev_id)
+{
+	u32 IrqStatus;  
+	/* Read pending interrupts */
+	IrqStatus = ioread32(vp->base_addr + 4);//read irq status from MM2S_DMASR register
+	iowrite32(IrqStatus | 0x00007000, vp->base_addr + 4);//clear irq status in MM2S_DMASR register
+	//(clearing is done by writing 1 on 13. bit in MM2S_DMASR (IOC_Irq)
+
+	/*Send a transaction*/
+	dma_simple_write(tx_phy_buffer, MAX_PKT_LEN, vp->base_addr); //My function that starts a DMA transaction
+	return IRQ_HANDLED;;
+}
+
+int dma_init(void __iomem *base_address)
+{
+	u32 reset = 0x00000004;
+	u32 IOC_IRQ_EN; 
+	u32 ERR_IRQ_EN;
+	u32 MM2S_DMACR_reg;
+	u32 en_interrupt;
+
+	IOC_IRQ_EN = 1 << 12; // this is IOC_IrqEn bit in MM2S_DMACR register
+	ERR_IRQ_EN = 1 << 14; // this is Err_IrqEn bit in MM2S_DMACR register
+
+	iowrite32(reset, base_address); // writing to MM2S_DMACR register. Seting reset bit (3. bit)
+
+	MM2S_DMACR_reg = ioread32(base_address); // Reading from MM2S_DMACR register inside DMA
+	en_interrupt = MM2S_DMACR_reg | IOC_IRQ_EN | ERR_IRQ_EN;// seting 13. and 15.th bit in MM2S_DMACR
+	iowrite32(en_interrupt, base_address); // writing to MM2S_DMACR register  
+	return 0;
+}
+
+u32 dma_simple_write(dma_addr_t TxBufferPtr, u32 max_pkt_len, void __iomem *base_address) {
+	u32 MM2S_DMACR_reg;
+
+	MM2S_DMACR_reg = ioread32(base_address); // READ from MM2S_DMACR register
+
+	iowrite32(0x1 |  MM2S_DMACR_reg, base_address); // set RS bit in MM2S_DMACR register (this bit starts the DMA)
+
+	iowrite32((u32)TxBufferPtr, base_address + 24); // Write into MM2S_SA register the value of TxBufferPtr.
+	// With this, the DMA knows from where to start.
+
+	iowrite32(max_pkt_len, base_address + 40); // Write into MM2S_LENGTH register. This is the length of a tranaction.
+	// In our case this is the size of the image (640*480*4)
+	return 0;
+}
+
+
+
+//***************************************************
+// INIT AND EXIT FUNCTIONS OF THE DRIVER
+
+static int __init image_init(void)
+{
+
+	int ret = 0;
+	int i = 0;
+
+	printk(KERN_INFO "image_init: Initialize Module \"%s\"\n", DEVICE_NAME);
+	ret = alloc_chrdev_region(&my_dev_id, 0, 1, "VGA_region");
+	if (ret)
+	{
+		printk(KERN_ALERT "image_init: Failed CHRDEV!\n");
+		return -1;
+	}
+	printk(KERN_INFO "image_init: Successful CHRDEV!\n");
+	my_class = class_create(THIS_MODULE, "VGA_drv");
+	if (my_class == NULL)
+	{
+		printk(KERN_ALERT "image_init: Failed class create!\n");
+		goto fail_0;
+	}
+	printk(KERN_INFO "image_init: Successful class chardev1 create!\n");
+	my_device = device_create(my_class, NULL, MKDEV(MAJOR(my_dev_id),0), NULL, "image");
+	if (my_device == NULL)
+	{
+		goto fail_1;
+	}
+
+	printk(KERN_INFO "image_init: Device created\n");
+
+	my_cdev = cdev_alloc();	
+	my_cdev->ops = &my_fops;
+	my_cdev->owner = THIS_MODULE;
+	ret = cdev_add(my_cdev, my_dev_id, 1);
+	if (ret)
+	{
+		printk(KERN_ERR "image_init: Failed to add cdev\n");
+		goto fail_2;
+	}
+	printk(KERN_INFO "image_init: Module init done\n");
+
+	tx_vir_buffer = dma_alloc_coherent(NULL, MAX_PKT_LEN, &tx_phy_buffer, GFP_DMA | GFP_KERNEL);
+	if(!tx_vir_buffer){
+		printk(KERN_ALERT "image_init: Could not allocate dma_alloc_coherent for img");
+		goto fail_3;
+	}
+	else
+		printk("image_init: Successfully allocated memory for dma transaction buffer\n");
+	for (i = 0; i < MAX_PKT_LEN/4;i++)
+		tx_vir_buffer[i] = 0x00000000;
+	printk(KERN_INFO "image_init: DMA memory reset.\n");
+	return platform_driver_register(&image_driver);
+
+fail_3:
+	cdev_del(my_cdev);
+fail_2:
+	device_destroy(my_class, MKDEV(MAJOR(my_dev_id),0));
+fail_1:
+	class_destroy(my_class);
+fail_0:
+	unregister_chrdev_region(my_dev_id, 1);
+	return -1;
+
+} 
+
+static void __exit image_exit(void)  		
+{
+	//Reset DMA memory
+	int i =0;
+	for (i = 0; i < MAX_PKT_LEN/4; i++) 
+		tx_vir_buffer[i] = 0x00000000;
+	printk(KERN_INFO "image_exit: DMA memory reset\n");
+
+	// Exit Device Module
+	platform_driver_unregister(&image_driver);
+	cdev_del(my_cdev);
+	device_destroy(my_class, MKDEV(MAJOR(my_dev_id),0));
+	class_destroy(my_class);
+	unregister_chrdev_region(my_dev_id, 1);
+	dma_free_coherent(NULL, MAX_PKT_LEN, tx_vir_buffer, tx_phy_buffer);
+	printk(KERN_INFO "image_exit: Exit device module finished\"%s\".\n", DEVICE_NAME);
+}
+
+module_init(image_init);
+module_exit(image_exit);
+
---
+++ Drivers/mlp_driver/mlp_driver.c
@@ -0,0 +1,285 @@
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
+#include <linux/device.h>
+#include <linux/of.h>
+
+#include <linux/io.h> //iowrite ioread
+#include <linux/slab.h>//kmalloc kfree
+#include <linux/platform_device.h>//platform driver
+#include <linux/of.h>//of match table
+#include <linux/ioport.h>//ioremap
+
+#define BUFF_SIZE 20
+#define DRIVER_NAME "mlp"
+MODULE_LICENSE("Dual BSD/GPL");
+
+struct mlp_info {
+  unsigned long mem_start;
+  unsigned long mem_end;
+  void __iomem *base_addr;
+};
+
+dev_t my_dev_id;
+static struct class *my_class;
+static struct device *my_device;
+static struct cdev *my_cdev;
+static struct mlp_info *lp = NULL;
+
+int endRead = 0;
+
+
+static int mlp_probe(struct platform_device *pdev);
+static int mlp_remove(struct platform_device *pdev);
+int mlp_open(struct inode *pinode, struct file *pfile);
+int mlp_close(struct inode *pinode, struct file *pfile);
+ssize_t mlp_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+ssize_t mlp_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
+static int __init mlp_init(void);
+static void __exit mlp_exit(void);
+
+struct file_operations my_fops =
+{
+	.owner = THIS_MODULE,
+	.open = mlp_open,
+	.read = mlp_read,
+	.write = mlp_write,
+	.release = mlp_close,
+};
+
+static struct of_device_id mlp_of_match[] = {
+  { .compatible = "mlp", },
+  { /* end of list */ },
+};
+
+static struct platform_driver mlp_driver = {
+  .driver = {
+    .name = DRIVER_NAME,
+    .owner = THIS_MODULE,
+    .of_match_table	= mlp_of_match,
+  },
+  .probe		= mlp_probe,
+  .remove		= mlp_remove,
+};
+
+
+MODULE_DEVICE_TABLE(of, mlp_of_match);
+
+static int mlp_probe(struct platform_device *pdev)
+{
+  struct resource *r_mem;
+  int rc = 0;
+  r_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+  if (!r_mem) {
+    printk(KERN_ALERT "Faimlp to get resource\n");
+    return -ENODEV;
+  }
+  lp = (struct mlp_info *) kmalloc(sizeof(struct mlp_info), GFP_KERNEL);
+  if (!lp) {
+    printk(KERN_ALERT "Could not allocate mlp device\n");
+    return -ENOMEM;
+  }
+
+  lp->mem_start = r_mem->start;
+  lp->mem_end = r_mem->end;
+  //printk(KERN_INFO "Start address:%x \t end address:%x", r_mem->start, r_mem->end);
+
+  if (!request_mem_region(lp->mem_start,lp->mem_end - lp->mem_start + 1,	DRIVER_NAME))
+  {
+    printk(KERN_ALERT "Could not lock memory region at %p\n",(void *)lp->mem_start);
+    rc = -EBUSY;
+    goto error1;
+  }
+
+  lp->base_addr = ioremap(lp->mem_start, lp->mem_end - lp->mem_start + 1);
+  if (!lp->base_addr) {
+    printk(KERN_ALERT "Could not allocate memory\n");
+    rc = -EIO;
+    goto error2;
+  }
+
+  printk(KERN_WARNING "mlp platform driver registered\n");
+  return 0;//ALL OK
+
+error2:
+  release_mem_region(lp->mem_start, lp->mem_end - lp->mem_start + 1);
+error1:
+  return rc;
+}
+
+static int mlp_remove(struct platform_device *pdev)
+{
+  printk(KERN_WARNING "mlp platform driver removed\n");
+  iowrite32(0, lp->base_addr);
+  iounmap(lp->base_addr);
+  release_mem_region(lp->mem_start, lp->mem_end - lp->mem_start + 1);
+  kfree(lp);
+  return 0;
+}
+
+
+
+int mlp_open(struct inode *pinode, struct file *pfile) 
+{
+		printk(KERN_INFO "Succesfully opened mlp\n");
+		return 0;
+}
+
+int mlp_close(struct inode *pinode, struct file *pfile) 
+{
+		printk(KERN_INFO "Succesfully closed mlp\n");
+		return 0;
+}
+
+ssize_t mlp_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
+{
+	int ret;
+	int len = 0;
+	u32 ready_val = 0;
+	u32 cl_num;
+	int i = 0;
+	char buff[BUFF_SIZE];
+	if (endRead){
+		endRead = 0;
+		return 0;
+	}
+
+	ready_val = ioread32(lp->base_addr + 4); // ready register is at address 4
+	//if(wait_event_interruptible(readQ, (ready_val == 1)))
+	//	return -ERESTARTSYS;
+	
+	while(!ready_val) {
+			ready_val = ioread32(lp->base_addr + 4);
+	}
+	ready_val = 0;
+	
+	cl_num = ioread32(lp->base_addr + 12); // cl_num reg at addr 12
+
+	buff[0]= '0';
+	buff[1]= 'x';
+	buff[2]= '3';
+	buff[3] = '0' + cl_num;
+	//alternative
+	//char digits[10] = {'0','1','2','3','4','5','6','7','8','9'};
+	//buff[3]=digits[cl_num];
+
+	len=4;
+	ret = copy_to_user(buffer, buff, len);
+	if(ret)
+		return -EFAULT;
+	//printk(KERN_INFO "Succesfully read\n");
+	//wake_up_interruptible(&readQ);
+	endRead = 1;
+
+	return len;
+}
+
+ssize_t mlp_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
+{
+	char buff[BUFF_SIZE];
+	int ret = 0;
+	long int start_val=0;
+
+	ret = copy_from_user(buff, buffer, length);
+	if(ret)
+		return -EFAULT;
+	buff[length] = '\0';
+
+	// HEX  INPUT
+	if(buff[0] == '0' && (buff[1] == 'x' || buff[1] == 'X')) 
+	{
+		ret = kstrtol(buff+2,16,&start_val);
+	}
+	// BINARY INPUT
+	else if(buff[0] == '0'  && (buff[1] == 'b' || buff[1] == 'B')) 
+	{
+		ret = kstrtol(buff+2,2,&start_val);
+	}
+	// DECIMAL INPUT
+	else 
+	{
+		ret = kstrtol(buff,10,&start_val);
+	}
+
+	
+	if (!ret)
+	{
+		iowrite32((u32)start_val, lp->base_addr);
+		printk(KERN_INFO "Succesfully wrote value %#x",(u32)ready_val); 
+	}
+	else
+	{
+		printk(KERN_INFO "Wrong command format\n"); 
+	}
+
+	return length;
+}
+
+static int __init mlp_init(void)
+{
+   int ret = 0;
+
+   ret = alloc_chrdev_region(&my_dev_id, 0, 1, DRIVER_NAME);
+   if (ret){
+      printk(KERN_ERR "faimlp to register char device\n");
+      return ret;
+   }
+   printk(KERN_INFO "char device region allocated\n");
+
+   my_class = class_create(THIS_MODULE, "mlp_class");
+   if (my_class == NULL){
+      printk(KERN_ERR "faimlp to create class\n");
+      goto fail_0;
+   }
+   printk(KERN_INFO "class created\n");
+   
+   my_device = device_create(my_class, NULL, my_dev_id, NULL, DRIVER_NAME);
+   if (my_device == NULL){
+      printk(KERN_ERR "faimlp to create device\n");
+      goto fail_1;
+   }
+   printk(KERN_INFO "device created\n");
+
+	my_cdev = cdev_alloc();	
+	my_cdev->ops = &my_fops;
+	my_cdev->owner = THIS_MODULE;
+	ret = cdev_add(my_cdev, my_dev_id, 1);
+	if (ret)
+	{
+      printk(KERN_ERR "faimlp to add cdev\n");
+		goto fail_2;
+	}
+   printk(KERN_INFO "cdev added\n");
+   printk(KERN_INFO "Hello world\n");
+
+  return platform_driver_register(&mlp_driver);
+
+   fail_2:
+      device_destroy(my_class, my_dev_id);
+   fail_1:
+      class_destroy(my_class);
+   fail_0:
+      unregister_chrdev_region(my_dev_id, 1);
+   return -1;
+}
+
+static void __exit mlp_exit(void)
+{
+   platform_driver_unregister(&mlp_driver);
+   cdev_del(my_cdev);
+   device_destroy(my_class, my_dev_id);
+   class_destroy(my_class);
+   unregister_chrdev_region(my_dev_id,1);
+   printk(KERN_INFO "Goodbye, cruel world\n");
+}
+
+
+module_init(mlp_init);
+module_exit(mlp_exit);
